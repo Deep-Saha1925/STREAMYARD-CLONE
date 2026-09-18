66ddf6ccd808b6cf7fcfcb8bcb338cdd150fcc70
Deep-Saha1925 <dip23447@gmail.com>
2026-09-18 12:19:03 +0530

    Collecting streams and passing it

--- public/script.js
+++ public/script.js
@@ -1,7 +1,24 @@
 const startBtn = document.getElementById('startButton');
 const userVideo = document.getElementById('localVideo');
 
+const state = { media: null }
+
+startBtn.addEventListener('click', async () => {
+    const mediaRecorder = new MediaRecorder(state.media, {
+        audioBitsPerSecond: 128000,
+        videoBitsPerSecond: 2500000,
+        frameRate: 25
+    })
+
+    mediaRecorder.ondataavailable = (event) => {
+        
+    }
+
+    mediaRecorder.start();
+})
+
 window.addEventListener('load', async () => {
     const media = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    state.media = media;
     userVideo.srcObject = media;
 })
