5ba33fde569a7455415b9114aca8249e9ac4cbf1
Deep-Saha1925 <dip23447@gmail.com>
2026-09-18 12:21:32 +0530

    Adding socket io

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import http from "http";
 import express from "express";
 import path from "path";
+import {Server as SocketIO} from "socket.io"
 
 const app = express();
 const server = http.createServer(app);
+const io = new SocketIO(server);
 
 app.use(express.static(path.join("public")));
 
+io.on("connection", (socket) => {
+    console.log("Socket connected");
+})
+
 server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port http://localhost:3000");
 })
